995be91d755f1005c0f64c76c93753dca7a40802
gdimitris <dimitrios.yiannakos@gmail.com>
2017-04-17 15:51:28 +0300

    Initial commit

---
+++ desktop/src/com/gdimitris/boxcrush/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdimitris.boxcrush.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdimitris.boxcrush.BoxCrush;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoxCrush(), config);
+	}
+}
---
+++ html/src/com/gdimitris/boxcrush/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.gdimitris.boxcrush.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.gdimitris.boxcrush.BoxCrush;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new BoxCrush();
+        }
+}
